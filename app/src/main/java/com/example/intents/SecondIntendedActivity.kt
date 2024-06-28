17761898d38812b183e03cc9a1adbdd0149855a4
ZeeshanAkhtar789 <zakhtar623@gmail.com>
2024-06-29 00:27:33 +0530

    Adding files of Android Development Projects

---
+++ app/src/main/java/com/example/intents/SecondIntendedActivity.kt
@@ -0,0 +1,27 @@
+package com.example.intents
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondIntendedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second_intende)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnIntents1 = findViewById<Button>(R.id.btnBack);
+        btnIntents1.setOnClickListener {
+            intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
